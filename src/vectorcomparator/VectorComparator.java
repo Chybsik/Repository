6615d90a7077251886bb36d7db275dbf99ab0542
Timur <Timur@Timur-PC>
2017-12-27 16:17:45 +0400

    VectorComparator 1.01

--- src/vectorcomparator/VectorComparator.java
+++ src/vectorcomparator/VectorComparator.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package vectorcomparator;
+import java.io.*;
+import static java.lang.System.out;
+import java.nio.file.*;
+import java.util.regex.*;
 
 /**
  *
@@ -16,6 +20,25 @@ public class VectorComparator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        String s = "";
+        Pattern pattern;
+        Matcher matcher;
+        String path_pattern;
+        
+        try{
+            s = new String(Files.readAllBytes(Paths.get("C:\\Users\\Timur\\Desktop\\SVG\\this_boy_loves_Christmas-4784.svg")));
+            //s = s.replace("\n", "").replace("\r", "");
+        }catch(IOException e){
+        }
+        
+        path_pattern = "<path.*(\n)*(\r)*/>";
+        pattern = Pattern.compile(path_pattern);
+        matcher = pattern.matcher(s);
+        if (matcher.find()){
+            out.println(matcher.group());
+        }else{
+            out.println("failure");
+        }
     }
-    
 }
