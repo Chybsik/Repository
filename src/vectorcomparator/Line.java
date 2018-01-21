21e8730378cfb331b2c01be8cf19f8fd64f321ec
Timur <Timur@Timur-PC>
2018-01-21 15:01:04 +0400

    VectorComparator 1.05

--- src/vectorcomparator/Line.java
+++ src/vectorcomparator/Line.java
@@ -14,17 +14,30 @@ public class Line {
     double posY1;
     double posX2;
     double posY2;
+    double length;
+    double k;
+    double weight=1;
     
     public Line(double posX1, double posY1, double posX2, double posY2){
         this.posX1 = posX1;
         this.posX2 = posX2;
         this.posY1 = posY1;
         this.posY2 = posY2;
+        
+        double length = Math.pow(posX1-posX2, 2)+Math.pow(posY1-posY2, 2);
+        double k = (posY2-posY1)/(posX2-posX1);
     }
     public Line(Vertex v1, Vertex v2){
         this.posX1 = v1.posX;
         this.posX2 = v2.posX;
         this.posY1 = v1.posY;
         this.posY2 = v2.posY;
+        
+        double length = Math.sqrt(Math.pow(posX1-posX2, 2)+Math.pow(posY1-posY2, 2));
+        double k = (posY2-posY1)/(posX2-posX1);
+    }
+    public double CompareTo(Line line){
+        double x = 1 - Math.abs(line.length-this.length+line.k-this.k)/2;
+        return (x<0)? 0:x;
     }
 }
--- src/vectorcomparator/VectorComparator.java
+++ src/vectorcomparator/VectorComparator.java
@@ -22,20 +22,5 @@ public class VectorComparator {
         //String p = "C:\\temp\\santa-claus.svg";
         //List<Vector> vector = new ArrayList<Vector>();
         //vector = SVGDestructor.Destruct(p);
-        Line line1 = new Line(0,0,20,35);
-        Line line2 = new Line(10,20, 40,0);
-        System.out.print(LineCompare(line1,line2));
-    }
-    public static double LineCompare(Line line1, Line line2){
-        double biss1 = Math.sqrt(Math.pow(line1.posX1-line1.posX2, 2)+Math.pow(line1.posY1-line1.posY2, 2));
-        double sin1 = (line1.posY1-line1.posY2)/biss1;
-        double cos1 = (line1.posX1-line1.posX2)/biss1;
-        
-        double biss2 = Math.sqrt(Math.pow(line2.posX1-line2.posX2, 2)+Math.pow(line2.posY1-line2.posY2, 2));
-        double sin2 = (line2.posY1-line2.posY2)/biss2;
-        double cos2 = (line2.posX1-line2.posX2)/biss2;
-        
-        double x = 1 - Math.abs(sin1-sin2 + cos1-cos2 + line1.posX1-line2.posX1 + line1.posY1-line2.posY1)/4;
-        return (x<0)? 0:x;
     }
 }
